43a81b8ae50eff12fec1e83c91fe2e1f77e8e79c
syarram-BRONGA <86463290+syarram-BRONGA@users.noreply.github.com>
2021-11-02 10:27:36 +0530

    [SY] [SMEE-2269] Code format.

--- src/main/java/com/gap/sourcing/smee/dtos/resources/SmeeUserCreateResource.java
+++ src/main/java/com/gap/sourcing/smee/dtos/resources/SmeeUserCreateResource.java
@@ -6,8 +6,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
-import javax.validation.constraints.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 
 
 @Getter
--- src/test/unit/java/com/gap/sourcing/smee/steps/user/SmeeUserCreateValidationStepTest.java
+++ src/test/unit/java/com/gap/sourcing/smee/steps/user/SmeeUserCreateValidationStepTest.java
@@ -2,41 +2,25 @@ package com.gap.sourcing.smee.steps.user;
 
 import com.gap.sourcing.smee.contexts.SmeeUserContext;
 import com.gap.sourcing.smee.dtos.resources.SmeeUserCreateResource;
-import com.gap.sourcing.smee.dtos.responses.denodo.DenodoResponse;
 import com.gap.sourcing.smee.entities.SmeeUserType;
-import com.gap.sourcing.smee.enums.RequestAction;
-import com.gap.sourcing.smee.exceptions.ApiClientException;
 import com.gap.sourcing.smee.exceptions.GenericBadRequestException;
 import com.gap.sourcing.smee.exceptions.GenericUserException;
-import com.gap.sourcing.smee.exceptions.GenericUnknownActionException;
 import com.gap.sourcing.smee.repositories.SmeeUserTypeRepository;
 import com.gap.sourcing.smee.steps.Step;
-import com.gap.sourcing.smee.utils.Client;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.stubbing.Answer;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.client.ClientResponse;
-import org.springframework.web.reactive.function.client.WebClient;
 import providers.ResourceProvider;
-import reactor.core.publisher.Mono;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsNull.notNullValue;
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -49,14 +33,10 @@ class SmeeUserCreateValidationStepTest {
     @Mock
     SmeeUserTypeRepository smeeUserTypeRepository;
 
-    private SmeeUserType entity;
-    private SmeeUserStepManager smeeUserStepManager;
     private Step smeeUserCreateValidationStep;
 
     @BeforeEach
     void init() {
-        entity = new SmeeUserType();
-        entity.setUserType("Garment Vendor");
         smeeUserCreateValidationStep = new SmeeUserCreateValidationStep(smeeUserCreateResourceConversionStep, smeeUserTypeRepository);
     }
 
