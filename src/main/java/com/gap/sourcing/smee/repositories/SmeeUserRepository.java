81543fd6201606ef76e06c9609f64755c8f748c6
vprabhu <Vinayak_Prabhu@gap.com>
2022-03-30 16:58:21 +0530

    [VP] Removed unnecessary configs for replica

--- src/main/java/com/gap/sourcing/smee/repositories/SmeeUserRepository.java
+++ src/main/java/com/gap/sourcing/smee/repositories/SmeeUserRepository.java
@@ -2,16 +2,11 @@ package com.gap.sourcing.smee.repositories;
 
 import com.gap.sourcing.smee.entities.SmeeUser;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SmeeUserRepository extends JpaRepository<SmeeUser, Integer> {
 
     SmeeUser findSmeeUserByUserName(String userName);
-
-    //TODO: Included this to prove it's working, to be deleted once tested
-    @Query(value = "SELECT cast(DATABASEPROPERTYEX(DB_NAME(), 'Updateability') as varchar);", nativeQuery = true)
-    String checkReplica();
 
 }
